0848fe021dd1a10414d66badc2d1b6543264b6c2
Enzo Maruffa <enzomm1999@gmail.com>
2024-08-02 23:56:13 -0300

    chore: Updates to newer iOS boilerplates

---
+++ Ratagochi/Ratagochi/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Ratagochi
+//
+//  Created by Enzo Maruffa Moreira on 02/08/24.
+//
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ratagochi/Ratagochi/RatagochiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatagochiApp.swift
+//  Ratagochi
+//
+//  Created by Enzo Maruffa Moreira on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RatagochiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
